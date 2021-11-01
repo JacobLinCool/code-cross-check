2ecbcad0ac015a4765d8e1413859eff843e0ee2e
JacobLinCool <jacoblincool@gmail.com>
2021-11-02 01:10:14 +0800

    feat(supabase): add supabase

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const Koa = require("koa");
 const compress = require("koa-compress");
 const Router = require("koa-router");
@@ -5,6 +6,8 @@ const koaBody = require("koa-body");
 const mount = require("koa-mount");
 const static = require("koa-static");
 const { Checker } = require("testcase-x");
+const { createClient } = require("@supabase/supabase-js");
+const supabase = createClient("https://zibatrytslwkcndzjbuk.supabase.co", process.env.SUPABASE_TOKEN);
 
 const app = new Koa();
 
@@ -14,7 +17,12 @@ const apiRouter = new Router();
 
 apiRouter.post("/cross-check", async (ctx) => {
     const { body } = ctx.request;
-    console.log(body);
+    Object.keys(body).forEach((key) => (body[key] = body[key].trim()));
+    console.log(
+        Object.keys(body)
+            .map((key) => `${key}: ${body[key].length}`)
+            .join(", ")
+    );
     const StartTime = Date.now();
     const checker = new Checker();
     const result = await checker
@@ -24,6 +32,15 @@ apiRouter.post("/cross-check", async (ctx) => {
         .source(body.source_1)
         .go();
     console.log(`[Checker ${checker.id}] Time:`, Date.now() - StartTime, "ms");
+    const hash = md5(`${md5(body.testcase)}-${md5(body.preprocessor)}-${md5(body.source_0)}-${md5(body.source_1)}-${md5(JSON.stringify(result))}`);
+    console.log("HASH:", hash);
+    supabase
+        .from("records")
+        .insert([{ testcase: body.testcase, preprocessor: body.preprocessor, source_0: body.source_0, source_1: body.source_1, result, hash }])
+        .then(({ data, error }) => {
+            if (error) console.log("[Supabase Error]", error);
+            else console.log("[Supabase]", "Size: " + JSON.stringify(data).length);
+        });
     ctx.body = JSON.stringify(result, null, 2);
     ctx.type = "application/json";
 });
@@ -56,6 +73,10 @@ function requireFromString(src) {
     const m = new Module();
     m._compile(src, "");
     return m.exports;
+}
+
+function md5(str) {
+    return require("crypto").createHash("md5").update(str).digest("hex");
 }
 
 // #endregion
