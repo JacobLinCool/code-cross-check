1ab436cd899d1c82796b1ae3f6d29135133c513c
JacobLinCool <jacoblincool@gmail.com>
2021-11-03 22:32:58 +0800

    perf(front): minify html

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,10 @@
 import { defineConfig } from "vite";
 import monacoEditorPlugin from "vite-plugin-monaco-editor";
+import { minifyHtml } from "vite-plugin-html";
 
 export default defineConfig({
     base: "./",
     root: "./src-web",
-    build: {
-        outDir: "../web",
-    },
-    plugins: [
-        monacoEditorPlugin({
-            languageWorkers: ["typescript"],
-        }),
-    ],
+    build: { outDir: "../web" },
+    plugins: [monacoEditorPlugin({ languageWorkers: ["typescript"] }), minifyHtml()],
 });
