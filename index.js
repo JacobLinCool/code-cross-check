084c16b7f4fa0b567c9b956d160b103c14d6aa38
JacobLinCool <jacoblincool@gmail.com>
2021-11-06 17:58:07 +0800

    feat(core): prevent uncaught exception to crash the server

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 require("./server");
+
+process.on("uncaughtException", async (err) => {
+    console.log("\033[1;91m" + "[Uncaught Exception] " + "\033[0m" + err.stack);
+    console.log("\033[1;91m" + "[Uncaught Exception] " + "\033[0m" + "Memory:", process.memoryUsage());
+    console.log("\033[1;91m" + "[Uncaught Exception] " + "\033[0m" + "CPU:", process.cpuUsage());
+    console.log("\033[1;91m" + "[Uncaught Exception] " + "\033[0m" + "Uptime:", process.uptime(), "s");
+});
+
+process.on("close", async (code) => {
+    console.log("\033[1;94m" + "[Exit] " + "\033[0m" + "Code:", code);
+    console.log("\033[1;94m" + "[Exit] " + "\033[0m" + "Memory:", process.memoryUsage());
+    console.log("\033[1;94m" + "[Exit] " + "\033[0m" + "CPU:", process.cpuUsage());
+    console.log("\033[1;94m" + "[Exit] " + "\033[0m" + "Uptime:", process.uptime(), "s");
+});
